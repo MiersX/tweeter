55e14f9080c11df420592fb8ea81f4b250b56188
MiersX <michael.muirhead123@gmail.com>
2022-09-15 09:44:43 +0000

    added ajax get request, empty container expression, and imported timeago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,36 +4,13 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
 
 $( document ).ready(function() {
 
 
   const createTweetElement = function(tweetData) {
-
+   
 
     // Declare the target and add a class of tweet to maintain css styling
 
@@ -41,6 +18,8 @@ $( document ).ready(function() {
 
     // Using literals to generate an html markup and integrate user/form data
 
+
+  
   const htmlMarkup = `
   <header>
     <span class="avatar-and-name">
@@ -66,22 +45,34 @@ $( document ).ready(function() {
   // Loops through the tweets database and appends each tweet to the tweet-container
 
   const renderTweets = (tweets) => {
+    $('.tweet-container').empty();
+
     for (const tweet of tweets) {
       $('.tweet-container').append(createTweetElement(tweet))
     }
   }
   
-  renderTweets(data);
+  //renderTweets(data);
+
 
   // Bind the submit listener to the tweet-form element -> Prevent default behaviour -> jQuery / ajax post request.
+  
   $('.tweet-form').submit(function(event) {
     event.preventDefault();
 
-    $.post('/tweets', $(this).serialize());
+    $.post('/tweets', $(this).serialize())
   });
 
+  // Makes an ajax GET request. Calls the renderTweets as a callback on success.
 
+  const loadTweets = function() {
+    $.ajax('/tweets', {
+      method: 'GET',
+      dataType: 'JSON',
+      success: (tweets) => renderTweets(tweets),
+    });
+  };
 
-
-});
-
+  
+  loadTweets();
+  })
