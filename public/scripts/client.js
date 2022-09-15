ffb047faae5e0bce49a0d75a0314ac0320059198
MiersX <michael.muirhead123@gmail.com>
2022-09-15 05:42:34 +0000

    implemented .html() in client to render tweet object properly

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,14 +7,30 @@
 $( document ).ready(function() {
 
 
+ const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+  }
+
+
+
+
   const createTweetElement = function(tweetData) {
 
-  const $tweet = $(`<article class="tweet">Hello world</article>`);
+  const $tweet = $(`<article>`).addClass('tweet');
 
+ 
   const htmlMarkup = `
   <header>
-    <span>
-      <img src=${tweetData.user.avatars} alt="The avatar representing ${tweetData.user.handle}">
+    <span class="avatar-and-name">
+      <img src=${tweetData.user.avatars} class="user-avatar" alt="The avatar representing ${tweetData.user.handle}">
       ${tweetData.user.name}
     </span>
     <span class="handle">${tweetData.user.handle}</span>
@@ -30,32 +46,18 @@ $( document ).ready(function() {
   </footer>  
   `;
 
-
-
-
-
-
-    return $tweet;
+    return $tweet.html(htmlMarkup);
   }
+
+  const $tweet = createTweetElement(tweetData)
+  console.log($tweet);
+  $('.tweet-container').append($tweet);
 
   
 
 });
 
-/*
-{
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
 
-*/
 
 
 /*
