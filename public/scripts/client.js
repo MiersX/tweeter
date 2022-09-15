83df0a9eb2ff111b45723d736a9444d6700426e1
MiersX <michael.muirhead123@gmail.com>
2022-09-15 07:21:05 +0000

    adding ajax and submit event listener

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,22 +4,32 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$( document ).ready(function() {
-
-
- const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   }
+]
 
-
+$( document ).ready(function() {
 
 
   const createTweetElement = function(tweetData) {
@@ -49,34 +59,20 @@ $( document ).ready(function() {
     return $tweet.html(htmlMarkup);
   }
 
-  const $tweet = createTweetElement(tweetData)
-  console.log($tweet);
-  $('.tweet-container').append($tweet);
-
   
+  const renderTweets = (tweets) => {
+    for (const tweet of tweets) {
+      $('.tweet-container').append(createTweetElement(tweet))
+    }
+  }
+  
+  renderTweets(data);
+
+
+  $('.tweet-form')
+
+
+
 
 });
-
-
-
-
-/*
-          <header>
-            <span>
-              <i class="fa-solid fa-user-astronaut"></i>
-            Name
-          </span>
-            <span class="handle">@Handle</span>
-          </header>
-          <p>This is an example tweet: I love banana pancakes</p>
-          <footer>
-            <span>X Days ago</span>
-            <span class="footIcon">
-              <i class="fa-solid fa-flag"></i>
-              <i class="fa-solid fa-crow"></i>
-              <i class="fa-solid fa-heart"></i>
-            </span>
-          </footer>
-*/
-
 
