d360b776287b7c13e3fa0e5fbba2c46513304fce
MiersX <michael.muirhead123@gmail.com>
2022-09-16 05:32:38 +0000

    implemented escape function to escape unwanted user inputs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,9 +4,15 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-
 $( document ).ready(function() {
+
+  // Function to escape malicious or accidental code being evaluated.
+
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
 
   const createTweetElement = function(tweetData) {
@@ -16,21 +22,19 @@ $( document ).ready(function() {
 
   const $tweet = $(`<article>`).addClass('tweet');
 
-    // Using literals to generate an html markup and integrate user/form data
-
-
+    // Using literals to generate an html markup and integrate user/form data. Escaped from all instances where users could add script.
   
   const htmlMarkup = `
   <header>
     <span class="avatar-and-name">
-      <img src=${tweetData.user.avatars} class="user-avatar" alt="The avatar representing ${tweetData.user.handle}">
-      ${tweetData.user.name}
+      <img src=${escape(tweetData.user.avatars)} class="user-avatar" alt="The avatar representing ${escape(tweetData.user.handle)}">
+      ${escape(tweetData.user.name)}
     </span>
-    <span class="handle">${tweetData.user.handle}</span>
+    <span class="handle">${escape(tweetData.user.handle)}</span>
   </header>
-  <p>${tweetData.content.text}</p>
+  <p>${escape(tweetData.content.text)}</p>
   <footer>
-    <span>${tweetData.created_at}</span>
+    <span>${escape(tweetData.created_at)}</span>
     <span class="footIcon">
       <i class="fa-solid fa-flag"></i>
       <i class="fa-solid fa-crow"></i>
@@ -42,25 +46,37 @@ $( document ).ready(function() {
     return $tweet.html(htmlMarkup);
   }
 
-  // Loops through the tweets database and appends each tweet to the tweet-container
+  // Empty the container to avoid rendering the same tweets again. Loop through the tweets database. Prepends each tweet to the tweet-container
 
   const renderTweets = (tweets) => {
     $('.tweet-container').empty();
 
     for (const tweet of tweets) {
-      $('.tweet-container').append(createTweetElement(tweet))
+      $('.tweet-container').prepend(createTweetElement(tweet))
     }
   }
   
-  //renderTweets(data);
-
-
-  // Bind the submit listener to the tweet-form element -> Prevent default behaviour -> jQuery / ajax post request.
+  
+  // Bind the submit listener to the tweet-form element -> Prevent default behaviour -> jQuery / ajax POST request.
   
   $('.tweet-form').submit(function(event) {
     event.preventDefault();
 
-    $.post('/tweets', $(this).serialize())
+    const textField = $(this).find("textarea");
+    const $userInput = $(this).find("textarea").val();
+    
+    // error handle if users tweet is over-limit or non-existant
+    if ($userInput.length > 140) {
+      alert("The length of your tweet has exceeded the 140 character limit!");
+      return;
+    }
+    if (!$userInput ) {
+      alert("You can't submit an empty tweet!");
+      return;
+    }
+    // Sends the form elements as a url-encoded string. 
+    // Success callback loads tweets, and resets the text-box and char-counter.
+    $.post('/tweets', $(this).serialize(), () => {loadTweets(); textField.val(""); $(".counter").text("140");})
   });
 
   // Makes an ajax GET request. Calls the renderTweets as a callback on success.
